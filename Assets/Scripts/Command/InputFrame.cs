0d877543c0f1ab255382f12fa61da12afc95c89e
setsuodu <339131001@qq.com>
2020-07-27 22:44:14 +0800

    新增移动的例子
    Signed-off-by: setsuodu <339131001@qq.com>

---
+++ Assets/Scripts/Command/InputFrame.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputFrame : MonoBehaviour
+{
+    static Queue<Vector3> buffers = new Queue<Vector3>();
+    Vector3 buff;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+        buff = transform.position;
+    }
+
+    //10f
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            // 命令模
+            Vector3 move = new Vector3(0, 0, 1);
+            ICommand command = new MoveCommand(move, transform);
+            CommandInvoker.AddCommand(command);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // 命令模
+            Vector3 move = new Vector3(0, 0, -1);
+            ICommand command = new MoveCommand(move, transform);
+            CommandInvoker.AddCommand(command);
+        }
+    }
+
+    //60f
+    void Update()
+    {
+        if (buffers.Count > 0)
+        {
+            buff = buffers.Dequeue();
+        }
+        transform.position = Vector3.Lerp(transform.position, buff, 0.1f);
+    }
+
+    public static void PlaceCube(Vector3 position, Transform cube)
+    {
+        Vector3 target = cube.position + position;
+        buffers.Enqueue(target);
+    }
+
+    public static void RemoveCube(Vector3 position, Transform cube)
+    {
+        Vector3 target = cube.position - position;
+        buffers.Enqueue(target);
+    }
+}
---
+++ Assets/Scripts/Command/MoveCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCommand : ICommand //接口要有实现
+{
+    Vector3 position;
+    Transform cube;
+
+    public MoveCommand(Vector3 position, Transform cube) 
+    {
+        this.position = position;
+        this.cube = cube;
+    }
+
+    public void Execute()
+    {
+        InputFrame.PlaceCube(position, cube);
+    }
+
+    public void Undo()
+    {
+        InputFrame.RemoveCube(position, cube);
+    }
+
+    public override string ToString()
+    {
+        return "PlaceCube\t" + position.x + ":" + position.y + ":" + position.z;
+    }
+}
