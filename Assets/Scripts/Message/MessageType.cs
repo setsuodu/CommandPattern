a805544d23e7edfefcae9036b4b18077e93c7ed5
setsuodu <339131001@qq.com>
2020-07-29 22:57:04 +0800

    计时
    Signed-off-by: setsuodu <339131001@qq.com>

--- Assets/Scripts/Message/MessageType.cs
+++
@@ -1,9 +0,0 @@
-﻿// 消息号
-public class MessageType
-{
-    public static byte EMPTY_MSG    = 0;
-    public static byte STRING_MSG   = 1;
-    public static byte MOVE_MSG     = 2;
-    public static byte COLOR_MSG    = 3;
-    public static byte JUMP_MSG     = 4;
-}
