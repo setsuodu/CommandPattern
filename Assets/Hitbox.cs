a09d8dd11b561c10a359d4e8caea14b8a1c8f3ea
setsuodu <339131001@qq.com>
2020-08-19 00:40:41 +0800

    添加hitbox例子
    Signed-off-by: setsuodu <339131001@qq.com>

--- Assets/Hitbox.cs
+++ Assets/Hitbox.cs
@@ -1,11 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEditor;
 
+[CustomEditor(typeof(Hitbox))]
+public class DemoEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector(); //显示默认所有参数
+
+        Hitbox demo = (Hitbox)target;
+
+        demo.frames = (int)GUILayout.HorizontalSlider(demo.frames, 0, demo.bounds.Length);
+
+        if (GUILayout.Button("AutoPlay"))
+        {
+            demo.AutoPlay();
+        }
+        if (GUILayout.Button("Previous"))
+        {
+            demo.frames--;
+            demo.frames = Mathf.Clamp(demo.frames, 0, demo.bounds.Length);
+            demo.Play(demo.frames);
+        }
+        if (GUILayout.Button("Next"))
+        {
+            demo.frames++;
+            demo.frames = Mathf.Clamp(demo.frames, 0, demo.bounds.Length);
+            demo.Play(demo.frames);
+        }
+    }
+}
+
+/// <summary>
+/// 技能碰撞体
+/// </summary>
 public class Hitbox : MonoBehaviour
 {
-    public float PosY;
-    public float PosZ;
-    public float Width;
-    public float Height;
+    public Bounds[] bounds;//手动配
+    public int frames;
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.parent.transform.Translate(Vector3.right * Time.deltaTime);
+    }
+
+    public void Play(int frame)
+    {
+        transform.localPosition = bounds[frame - 1].center;
+        transform.localScale = bounds[frame - 1].size;
+    }
+
+    public async void AutoPlay()
+    {
+        while (frames < bounds.Length)
+        {
+            frames++;
+            await Task.Delay(TimeSpan.FromSeconds(0.1f));
+            Play(frames);
+        }
+    }
 }
